ac7268663fe6be0fea0ccb2725a736a231909c26
Erick <erickaleruano@gmail.com>
2021-10-24 17:20:23 -0600

    Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Page from "./pages/Page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/page">Page</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/page">
+              <Page />
+            </Route>
+            <Route path="/">
+              Home
+            </Route>
+          </Switch>
+        </Router>
+      </main>
+    </>
   );
 }
 
---
+++ src/pages/Page/index.jsx
@@ -0,0 +1,3 @@
+const Page = () => "Page";
+
+export default Page;
