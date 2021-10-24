1988320d13b0cd40bfdcf460c25f6509fd9ba138
Erick <erickaleruano@gmail.com>
2021-10-24 17:33:50 -0600

    Implement TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
 import Page from "./pages/Page";
 
 function App() {
   return (
-    <>
+    <Router>
       <header>
         <nav>
-          <ul>
+          <ul className="flex flex-row justify-left items-center h-8 bg-gray-900 text-white space-x-8 py-12 px-8">
             <li>
-              <a href="/page">Page</a>
+              <img src={logo} alt="Application Logo" className="h-14" />
+            </li>
+            <li>
+              <Link to="/page">Page</Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>
-        <Router>
-          <Switch>
-            <Route path="/page">
-              <Page />
-            </Route>
-            <Route path="/">
-              Home
-            </Route>
-          </Switch>
-        </Router>
+      <main className="p-8">
+        <Switch>
+          <Route path="/page">
+            <Page />
+          </Route>
+          <Route path="/">Home</Route>
+        </Switch>
       </main>
-    </>
+    </Router>
   );
 }
 
