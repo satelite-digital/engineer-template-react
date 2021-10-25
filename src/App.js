eff01316ba2ef5f5f416c6f41431d2b2ecd7c217
Erick <erickaleruano@gmail.com>
2021-10-24 18:38:16 -0600

    Add DaisyUI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import Page from "./pages/Page";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <header>
-        <nav>
-          <ul className="flex flex-row justify-left items-center h-8 bg-gray-900 text-white space-x-8 py-12 px-8">
-            <li>
-              <img src={logo} alt="Application Logo" className="h-14" />
-            </li>
-            <li>
-              <Link to="/page">Page</Link>
-            </li>
-          </ul>
-        </nav>
+      <header className="p-8">
+        <div class="navbar shadow-lg bg-neutral text-neutral-content rounded-box">
+          <div class="flex-none px-2 mx-2">
+            <img src={logo} className="h-14" />
+          </div>
+          <div class="flex-1 px-2 mx-2">
+            <div class="items-stretch hidden lg:flex">
+              <Link class="btn btn-ghost btn-sm rounded-btn" to="/page">
+                Page
+              </Link>
+            </div>
+          </div>
+          <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+              <div class="avatar">
+                <div class="rounded-full w-12 h-12">
+                  <img
+                    src="https://i.pravatar.cc/100"
+                    alt="Avatar"
+                  />
+                </div>
+              </div>
+            </button>
+          </div>
+        </div>
       </header>
       <main className="p-8">
         <Switch>
           <Route path="/page">
             <Page />
           </Route>
-          <Route path="/">Home</Route>
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </main>
     </Router>
--- src/pages/Page/index.jsx
+++ src/pages/Page/index.jsx
@@ -1,3 +1,30 @@
-const Page = () => "Page";
+const Page = () => {
+  const cards = [0, 1, 2, 3, 4, 5, 6];
+  return (
+    <div className="grid gap-4 grid-cols-3">
+      {cards.map((card) => (
+        <div key={card} className="my-1 px-1 card text-center shadow-xl bg-neutral ">
+          <figure class="px-10 pt-10">
+            <img
+              src="https://picsum.photos/id/1005/400/250"
+              class="rounded-xl"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">shadow, center, padding</h2>
+            <p>
+              Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
+              Sit sit necessitatibus veritatis sed molestiae voluptates incidunt
+              iure sapiente.
+            </p>
+            <div class="justify-center card-actions">
+              <button class="btn btn-outline btn-accent">More info</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Page;
